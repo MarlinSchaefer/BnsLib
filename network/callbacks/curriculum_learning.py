14dd8245a1a4acc5dbaa5a68a09deeebbabf521f
Marlin Schäfer <mb.schaefer@gmx.de>
2020-09-25 11:12:13 +0200

    The CurriculumLearningScheduler can now monitor any quantity.

--- network/callbacks/curriculum_learning.py
+++ network/callbacks/curriculum_learning.py
@@ -14,13 +14,21 @@ class SnrCurriculumLearningScheduler(keras.callbacks.Callback):
     lower_at : {int or float, 10}
         Specifies when the SNR should be lowered. If set to an integer
         the SNR will be lowered after that number of epochs regardless
-        of the loss. When set to a float the SNR will be lowered once
-        the total validation loss drops below this value.
+        of the monitored quantity. When set to a float the SNR will be
+        lowered once the monitored quantity drops below this value.
     lower_by : {float, 5.}
         The value by which the SNR should be lowered.
     min_snr : {float, 5.}
         The SNR-value at which the training SNR will not be lowered
         anymore.
+    monitor : {str, 'val_loss'}
+        The quantity to monitor. Has to be a key of the logs that are
+        produced by Keras. (i.e. any loss or any metric)
+    mode : {'min' or 'max', 'min'}
+        When to schedule a new training target. If set to 'min' a new
+        target will be set when the monitored quantity falls below the
+        given threshold value. If set to 'max' a new target will be set
+        when the monitored quantity rises above the given threshold.
     
     Notes
     -----
@@ -32,11 +40,15 @@ class SnrCurriculumLearningScheduler(keras.callbacks.Callback):
     -The `rescale` method must take a list of length 2 as argument. The
      list contains the new target value.
     """
-    def __init__(self, generator, lower_at=10, lower_by=5., min_snr=5.):
+    def __init__(self, generator, lower_at=10, lower_by=5., min_snr=5.,
+                 monitor='val_loss', mode='min'):
         self.generator = generator
         self.lower_at = lower_at
         self.lower_by = lower_by
         self.min_snr = min_snr
+        self.monitor = monitor
+        assert mode.lower() in ['min', 'max']
+        self.mode = mode.lower()
     
     def on_epoch_begin(self, epoch, logs=None):
         print("Training with target: {}".format(self.generator.target))
@@ -46,8 +58,13 @@ class SnrCurriculumLearningScheduler(keras.callbacks.Callback):
         if isinstance(self.lower_at, int):
             lower = (epoch > 0) and (epoch % self.lower_at == 0)
         elif isinstance(self.lower_at, float):
-            loss = logs.get('val_loss', 0.)
-            lower = (loss < self.lower_at)
+            monitor = logs.get(self.monitor, 0.)
+            if self.mode == 'min':
+                lower = (monitor < self.lower_at)
+            elif self.mode == 'max':
+                lower = (monitor > self.lower_at)
+            else:
+                raise RuntimeError
         if lower:
             target = self.generator.target
             minsnr = safe_min(target)
