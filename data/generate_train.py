907dc1d02f31642fbc1a35b90d202863e3a120c6
Marlin Schäfer <mb.schaefer@gmx.de>
2021-02-05 11:05:42 +0100

    Adjusted the WaveformGetter to always give an option to use multiprocessing.
    Changes:
    -Merged generate and generate_mp of WaveformGetter
    -Added support to adjust the merger time by giving a parameter
     'tc'.
    -Added an implementation of a conditional variational autoencoder
     (CVAE) to the networks package. (Based on Keras, still might have
     a few bugs.

--- data/generate_train.py
+++ data/generate_train.py
@@ -143,10 +143,59 @@ def multi_noise_worker(length, delta_t, psd_name, flow, number, seed,
     output.put(ret)
 
 class WaveformGetter(object):
+    """Class to generate waveforms from given parameters. It can only
+    generate as many waveforms as there are values for each parameter in
+    the variable_params attribute.
+    
+    Arguments
+    ---------
+    variable_params : {None or dict, None}
+        The table containing the waveform parameters. These may include
+        all parameters accepted by pycbc.waveform.generate_td_waveform
+        as well as 'ra', 'dec' and 'pol' to specify a sky-position and
+        'tc' to place the merger time. The table is given as a
+        dictionary where the keys are the parameter names and the values
+        are iterables of floats that specify the value for each
+        waveform.
+    static_params : {None or dict, None}
+        Parameters that are constant for all waveforms. These may
+        commonly include the waveform approximant, the delta_t or the
+        lower frequency bound. The keys are the parameter names and may
+        contain any keys that could be put into the variable_params as
+        well.
+    domain : {'time' or 'freq', 'time'}
+        Whether to generate the waveforms in the time ('time') or
+        frequency ('freq') domain. [May be abbreviated to 't' and 'f'
+        respectifevly]
+    detectors : {None or str or list of str, 'H1'}
+        A list of detectors onto which the waveform is projected. If set
+        to None, the two polarizations of the waveform will be
+        generated. Detector names must be known to
+        pycbc.detector.Detector.
+    
+    Usage
+    -----
+    Generate a set of sources with different component masses from the
+    same sky-location:
+    >>> from BnsLib.data import WaveformGetter
+    >>> import numpy
+    >>> variable_params = {}
+    >>> variable_params['mass1'] = numpy.random.uniform(15., 50., size=10)
+    >>> variable_params['mass2'] = numpy.random.uniform(15., 50., size=10)
+    >>> static_params = {}
+    >>> static_params['approximant'] = 'TaylorF2'
+    >>> static_params['delta_t'] = 1. / 2048
+    >>> static_params['f_lower'] = 15.
+    >>> static_params['ra'] = numpy.pi
+    >>> static_params['dec'] = numpy.pi / 2
+    >>> static_params['distance'] = 1000.
+    >>> getter = WaveformGetter(variable_params=variable_params,\
+    >>>                         static_params=static_params,\
+    >>>                         detectors=['H1', 'L1'])
+    >>> waves = getter.generate(verbose=False)
+    """
     def __init__(self, variable_params=None, static_params=None,
-                 num_processes=1, domain='time', detectors='H1'):
-        self.initialized = False
-        self._num_processes = num_processes
+                 domain='time', detectors='H1'):
         self.variable_params = variable_params
         self.static_params = static_params
         self.domain = domain
@@ -164,7 +213,7 @@ class WaveformGetter(object):
             return len(self.variable_params[key])
     
     def __getitem__(self, index):
-        return self.generate(index=index)
+        return self.generate(index=index, workers=0, verbose=False)
     
     def __next__(self):
         if self._it_index < len(self):
@@ -177,11 +226,9 @@ class WaveformGetter(object):
     def __iter__(self):
         return self
     
-    def generate(self, index=None, single_detector_as_list=True):
+    def generate(self, index=None, single_detector_as_list=True,
+                 workers=None, verbose=True):
         """Generates one or multiple waveforms.
-        
-        TODO:
-        -Maybe merge this with generate_mp
         
         Arguments
         ---------
@@ -196,6 +243,13 @@ class WaveformGetter(object):
             use a dictionary. If this option is set to true, only the
             value of the dictionary will be returned when a single
             detector is used.
+        worker : {None or int >= 0, None}
+            How many processes to spawn to generate the waveforms. Set
+            to None in order to use as many processes as there are CPU
+            cores available. Set to 0 to disable multiprocessing.
+            (Turning off multiprocessing is useful for debugging.)
+        verbose : {bool, True}
+            Print a pogressbar for the waveform generation.
         
         Returns
         -------
@@ -211,121 +265,6 @@ class WaveformGetter(object):
             True and only a single detector is provided the function
             will return just the waveform and no dictionary.
         """
-        was_int = False
-        if index is None:
-            index = slice(None, None)
-        if isinstance(index, int):
-            index = slice(index, index+1)
-            was_int = True
-        
-        if self.detectors is None:
-            keys = ['plus', 'cross']
-        else:
-            keys = [det.name for det in self.detectors]
-            
-            ra_key = None
-            if 'ra' in self.variable_params or 'ra' in self.static_params:
-                ra_key = 'ra'
-            elif 'right_ascension' in self.variable_params or 'right_ascension' in self.static_params:
-                ra_key = 'right_ascension'
-            if ra_key is None:
-                ra_key = 'right_ascension'
-                self.variable_params[ra_key] = [np.nan for _ in range(len(self))]
-                msg = 'right_ascension was set neither in '
-                msg += 'variable_params nor in static_params. When '
-                msg += 'detectors are specified right_ascension should '
-                msg += 'be set manually. Will use the optimal source '
-                msg += 'right_ascensions instead.'
-                warnings.warn(msg, RuntimeWarning)
-            
-            dec_key = None
-            if 'dec' in self.variable_params or 'dec' in self.static_params:
-                dec_key = 'dec'
-            elif 'declination' in self.variable_params or 'declination' in self.static_params:
-                dec_key = 'declination'
-            if dec_key is None:
-                dec_key = 'declination'
-                self.variable_params[dec_key] = [np.nan for _ in range(len(self))]
-                msg = 'declination was set neither in '
-                msg += 'variable_params nor in static_params. When '
-                msg += 'detectors are specified declination should '
-                msg += 'be set manually. Will use the optimal source '
-                msg += 'declinations instead.'
-                warnings.warn(msg, RuntimeWarning)
-            
-            pol_key = None
-            if 'pol' in self.variable_params or 'pol' in self.static_params:
-                pol_key = 'pol'
-            elif 'polarization' in self.variable_params or 'polarization' in self.static_params:
-                pol_key = 'polarization'
-            if pol_key is None:
-                pol_key = 'polarization'
-                self.static_params[pol_key] = 0.
-                msg = 'polarization was set neither in '
-                msg += 'variable_params nor in static_params. When '
-                msg += 'detectors are specified declination should '
-                msg += 'be set manually. Will use polarization 0 '
-                msg += 'instead.'
-                warnings.warn(msg, RuntimeWarning)
-        
-        ret = {key: [] for key in keys}
-        for i in range(*index.indices(len(self))):
-            wave_kwargs = {}
-            for key, val in self.static_params.items():
-                wave_kwargs[key] = val
-            for key, val in self.variable_params.items():
-                wave_kwargs[key] = val[i]
-            if self.domain == 'time':
-                hp, hc = get_td_waveform(**wave_kwargs)
-            elif self.domain == 'frequency':
-                hp, hc = get_fd_waveform(**wave_kwargs)
-            if self.detectors is None:
-                ret['plus'].append(self.transform(hp))
-                ret['cross'].append(self.transform(hc))
-            else:
-                if ra_key not in self.static_params:
-                    if len(self.variable_params[ra_key]) <= i:
-                        for _ in range(i+1-len(self.variable_params[ra_key])):
-                            self.variable_params[ra_key].append(np.nan)
-                if dec_key not in self.static_params:
-                    if len(self.variable_params[dec_key]) <= i:
-                        for _ in range(i+1-len(self.variable_params[dec_key])):
-                            self.variable_params[dec_key].append(np.nan)
-                #FIXME: Cannot have the same source at different
-                #       sky-positions
-                for det in self.detectors:
-                    st = float(hp.start_time)
-                    calc_opt = {ra_key: np.isnan(wave_kwargs[ra_key]),
-                                dec_key: np.isnan(wave_kwargs[dec_key])}
-                    if any(list(calc_opt.values())):
-                        opt_ra, opt_dec = det.optimal_orientation(st)
-                        if calc_opt[ra_key]:
-                            wave_kwargs[ra_key] = opt_ra
-                            if ra_key in self.variable_params:
-                                self.variable_params[ra_key][i] = opt_ra
-                        if calc_opt[dec_key]:
-                            wave_kwargs[dec_key] = opt_dec
-                            if dec_key in self.variable_params:
-                                self.variable_params[dec_key][i] = opt_dec
-                    fp, fc = det.antenna_pattern(wave_kwargs[ra_key],
-                                                 wave_kwargs[dec_key],
-                                                 wave_kwargs[pol_key],
-                                                 st)
-                    h = fp * hp + fc * hc
-                    ret[det.name].append(self.transform(h))
-        
-        if was_int:
-            ret = {key: val[0] for (key, val) in ret.items()}
-        
-        if self.detectors is None:
-            return ret
-        
-        if single_detector_as_list and len(self.detectors) == 1:
-            return ret[self.detectors[0].name]
-        return ret
-    
-    def generate_mp(self, index=None, single_detector_as_list=True,
-                workers=None, verbose=True):
         if index is None:
             index = slice(None, None)
         was_int = False
@@ -335,8 +274,6 @@ class WaveformGetter(object):
         
         if workers is None:
             workers = mp.cpu_count()
-        if workers == 0:
-            return self.generate(single_detector_as_list=single_detector_as_list)
         
         indices = list(range(*index.indices(len(self))))
         
@@ -378,49 +315,73 @@ class WaveformGetter(object):
             detector_names = [det.name for det in self.detectors]
         
         #Generate the signals
-        waves_per_process = [len(indices) // workers] * workers
-        if sum(waves_per_process) < len(indices):
-            for i in range(len(indices) - sum(waves_per_process)):
-                waves_per_process[i] += 1
-        waves_per_process = np.cumsum(waves_per_process)
-        wpp = [0]
-        wpp.extend(waves_per_process)
-        
-        wave_boundaries = [slice(wpp[i], wpp[i+1]) for i in range(workers)]
-        wb = wave_boundaries
-        
-        bar = None
-        if verbose:
-            bar = mp_progress_tracker(len(indices),
-                                      name='Generating waveforms')
-        
-        jobs = []
-        output = mp.Queue()
-        for i in range(workers):
-            p = mp.Process(target=multi_wave_worker,
-                           args=(i,
-                                 wave_params[wb[i]],
-                                 projection_params[wb[i]],
-                                 detector_names,
-                                 self.transform,
-                                 self.domain,
-                                 bar,
-                                 output))
-            jobs.append(p)
-        
-        for p in jobs:
-            p.start()
-        
-        results = [output.get() for p in jobs]
-        
-        for p in jobs:
-            p.join()
-        
-        results.sort()
-        ret = DictList()
-        for pt in results:
-            ret.extend(pt[1])
-        ret = ret.as_dict()
+        if workers == 0:
+            if verbose:
+                progbar = progress_tracker(len(wave_params),
+                                           name='Generating waveforms')
+            if detector_names is None:
+                detectors = None
+            else:
+                detectors = [Detector(det) for det in detector_names]
+            ret = DictList()
+            for wav_params, proj_params in zip(wave_params, projection_params):
+                sig = signal_worker(wav_params,
+                                    proj_params,
+                                    detectors,
+                                    self.transform,
+                                    domain=self.domain)
+                for key, val in sig.items():
+                    if key in ret:
+                        ret.append(key, value=val)
+                    else:
+                        ret.append(key, value=[val])
+                if verbose:
+                    progbar.iterate()
+            ret = ret.as_dict()
+        else:
+            waves_per_process = [len(indices) // workers] * workers
+            if sum(waves_per_process) < len(indices):
+                for i in range(len(indices) - sum(waves_per_process)):
+                    waves_per_process[i] += 1
+            waves_per_process = np.cumsum(waves_per_process)
+            wpp = [0]
+            wpp.extend(waves_per_process)
+            
+            wave_boundaries = [slice(wpp[i], wpp[i+1]) for i in range(workers)]
+            wb = wave_boundaries
+            
+            bar = None
+            if verbose:
+                bar = mp_progress_tracker(len(indices),
+                                        name='Generating waveforms')
+            
+            jobs = []
+            output = mp.Queue()
+            for i in range(workers):
+                p = mp.Process(target=multi_wave_worker,
+                            args=(i,
+                                    wave_params[wb[i]],
+                                    projection_params[wb[i]],
+                                    detector_names,
+                                    self.transform,
+                                    self.domain,
+                                    bar,
+                                    output))
+                jobs.append(p)
+            
+            for p in jobs:
+                p.start()
+            
+            results = [output.get() for p in jobs]
+            
+            for p in jobs:
+                p.join()
+            
+            results.sort()
+            ret = DictList()
+            for pt in results:
+                ret.extend(pt[1])
+            ret = ret.as_dict()
         
         if was_int:
             ret = {key: val[0] for (key, val) in ret.items()}
@@ -431,6 +392,9 @@ class WaveformGetter(object):
         if single_detector_as_list and len(self.detectors) == 1:
             return ret[self.detectors[0].name]
         return ret
+    
+    #Legacy function
+    generate_mp = generate
     
     def get_params(self, index=None):
         if index is None:
@@ -451,35 +415,6 @@ class WaveformGetter(object):
     
     def transform(self, wav):
         return wav
-    
-    def assert_not_initialized(self, orig_func):
-        @wraps(orig_func)
-        def wrapper(*args, **kwargs):
-            if self.initialized:
-                return None
-            else:
-                return orig_func(*args, **kwargs)
-        return wrapper
-    
-    @property
-    def num_processes(self):
-        return self._num_processes
-    
-    @num_processes.setter
-    def num_processes(self, num_processes):
-        if num_processes is None:
-            num_processes = 1
-        
-        if not isinstance(num_processes, int):
-            msg = 'num_processes must be of type int. Got '
-            msg += f'{type(num_processes)} instead.'
-            raise TypeError(msg)
-        
-        if num_processes < 1:
-            msg = f'num_processes must be greater 0. Got {num_processes}.'
-            raise ValueError(msg)
-        
-        self._num_processes = num_processes
     
     @property
     def variable_params(self):
--- network/__init__.py
+++ network/__init__.py
@@ -1,3 +1,4 @@
 from BnsLib.network.layers import *
 from BnsLib.network import generators
 from BnsLib.network import callbacks
+from .cvae import *
