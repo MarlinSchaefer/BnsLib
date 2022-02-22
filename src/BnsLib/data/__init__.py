890ed81dba105811594d7dc16e5a416d8a64ba7d
Marlin Schäfer <mb.schaefer@gmx.de>
2022-02-22 16:48:06 +0100

    Fixed imports.

--- src/BnsLib/data/__init__.py
+++ src/BnsLib/data/__init__.py
@@ -1,6 +1,7 @@
 from .generate_train import WaveformGetter, ParamGenerator, WaveformGenerator,\
                             NoiseGenerator, WhiteNoiseGenerator
-from .multi_rate import multi_rate_sample, get_ideal_sample_rates
+from .multi_rate import multi_rate_sample, get_ideal_sample_rates,
+                        multi_rate_sample_fast
 from .transform import whiten, rescale_snr, rescale_signal, optimal_snr,\
                        optimal_network_snr, number_segments
 
@@ -8,4 +9,5 @@ from .transform import whiten, rescale_snr, rescale_signal, optimal_snr,\
 __all__ = ['WaveformGetter', 'ParamGenerator', 'WaveformGenerator',
            'NoiseGenerator', 'WhiteNoiseGenerator', 'multi_rate_sample',
            'get_ideal_sample_rates', 'whiten', 'rescale_snr', 'rescale_signal',
-           'optimal_snr', 'optimal_network_snr', 'number_segments']
+           'optimal_snr', 'optimal_network_snr', 'number_segments',
+           'multi_rate_sample_fast']
