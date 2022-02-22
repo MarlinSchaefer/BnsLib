e4bfeb5c59b1a2b158d96befcc5e6881c7281acf
Marlin Schäfer <mb.schaefer@gmx.de>
2022-02-22 16:48:47 +0100

    Fixed typo.

--- src/BnsLib/data/__init__.py
+++ src/BnsLib/data/__init__.py
@@ -1,6 +1,6 @@
 from .generate_train import WaveformGetter, ParamGenerator, WaveformGenerator,\
                             NoiseGenerator, WhiteNoiseGenerator
-from .multi_rate import multi_rate_sample, get_ideal_sample_rates,
+from .multi_rate import multi_rate_sample, get_ideal_sample_rates,\
                         multi_rate_sample_fast
 from .transform import whiten, rescale_snr, rescale_signal, optimal_snr,\
                        optimal_network_snr, number_segments
